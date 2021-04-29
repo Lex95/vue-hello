7fc72bca381e5001fe47f8425113e5e37cbfb9ca
Lex95 <andrea.lega1995@gmail.com>
2021-04-29 15:26:07 +0200

    risoluzione esercizio

---
+++ script.js
@@ -0,0 +1,12 @@
+const app = new Vue(
+    {
+        el: "#vueContainer",
+        data: {
+            myH1: "text-center",
+            message: "Welcome to Vue!",
+            // bonus
+            imgUrl: "https://www.capsforyou.it/wp-content/uploads/2020/01/AdobeStock_298163135.png",
+            myImg: "wider"
+        }
+    }
+)
